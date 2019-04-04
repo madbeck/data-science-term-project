76bfb0fb79deef90c36d1a66488178e10afee496
Maddie Becker <madeleine_becker@brown.edu>
2019-04-03 20:03:51 -0400

    Adding geojson and csv data for median income

--- generate_data.py
+++ generate_data.py
@@ -20,5 +20,5 @@ x = requests.get(url).json()
 print(x)
 
 # url = urllib.request.Request(base_URL, headers=parameters)
-#example = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY"
+# example = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY"
 
