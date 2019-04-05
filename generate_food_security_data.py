e1e2a9e4c83ac5ce9664cf367fd8a0afcc671938
Megs Malpani <megha.malpani@gmail.com>
2019-04-05 14:35:51 -0400

    Got nearby search working and collected nearest grocery store/fast food to zipcode data.

--- generate_food_security_data.py
+++ generate_food_security_data.py
@@ -1,24 +1,64 @@
 
 import requests
 import urllib
+import json
 
 key = "AIzaSyBeyECnXDJ-oLMp5WMySXDiBTmHNYNCfzU"
 base_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
-parameters = {
-	'location': "41.827268,-71.400564",
-	'radius': "1000",
-	'type': "restaurant",
-	#'keyword': "",
-	'key': key
-}
 
-url = base_URL + urllib.parse.urlencode(parameters)
-url = urllib.parse.unquote(url)
-print(url)
+#zipcode : {'groceries1km':#, 'groceries5km':#, ff1km ff5km}
+data = {}
 
-x = requests.get(url).json()
-print(x)
+with open('./lat_lon_by_zip_code.txt') as json_file:  
+	zipcode_info = json.load(json_file)
+	for zipcode in zipcode_info:
+		lat_long = zipcode_info[zipcode]
+		print(lat_long)
 
-# url = urllib.request.Request(base_URL, headers=parameters)
-# example = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY"
+		commands = [{'category': "1km_sm", 'radius':"1000", 'type':"supermarket", 'keyword':""},
+		{'category': "3km_sm", 'radius':"3000", 'type':"supermarket", 'keyword':""},
+		{'category': "1km_ff", 'radius':"1000", 'type':"", 'keyword':"Fast Food"},
+		{'category': "3km_ff", 'radius':"3000", 'type':"", 'keyword':"Fast Food"},]
 
+		food_access_data = {}
+		for command in commands:
+			parameters = {
+				'location': lat_long, #"41.827389, -71.399323",  #get latlong
+				'radius': command['radius'],
+				'type': command['type'],
+				'keyword': command['keyword'],
+				'key': key
+			}
+			url = base_URL + urllib.parse.urlencode(parameters)
+			url = urllib.parse.unquote(url)
+
+			response = requests.get(url).json()
+			results = response['results']
+			food_access_data[command['category']] = len(results)
+		print(food_access_data)
+		data[zipcode] = food_access_data
+
+print(data)
+
+with open('food_proximity_data.txt', 'w') as outfile:
+	json.dump(data, outfile)
+
+
+# parameters = {
+# 	'location': "41.827389, -71.399323", #lag_long, #get latlong
+# 	'radius': "1000",
+# 	'type': "",
+# 	# 'pagetoken': 1,
+# 	'keyword': "Fast Food",
+# 	'key': key
+# }
+
+# url = base_URL + urllib.parse.urlencode(parameters)
+# url = urllib.parse.unquote(url)
+# print(url)
+
+# response = requests.get(url).json()
+# restaurants = response['results']
+# print(len(restaurants))
+# for restaurant in restaurants:
+# 	print(restaurant['name'])
