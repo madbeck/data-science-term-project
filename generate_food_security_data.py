ce8f8babadf3a910f9e113f6b4aa2457e10401bf
wrichardson <wrichardson1218@gmail.com>
2019-04-19 22:24:49 -0400

    Checks if there is a price_level and returns it

--- generate_food_security_data.py
+++ generate_food_security_data.py
@@ -1,7 +1,7 @@
-
 import requests
 import urllib
 import json
+import numpy as np
 
 key = "AIzaSyBeyECnXDJ-oLMp5WMySXDiBTmHNYNCfzU"
 base_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
@@ -13,7 +13,7 @@ with open('./lat_lon_by_zip_code.txt') as json_file:
 	zipcode_info = json.load(json_file)
 	for zipcode in zipcode_info:
 		lat_long = zipcode_info[zipcode]
-		print(lat_long)
+		# print(lat_long)
 
 		commands = [{'category': "1km_sm", 'radius':"1000", 'type':"supermarket", 'keyword':""},
 		{'category': "3km_sm", 'radius':"3000", 'type':"supermarket", 'keyword':""},
@@ -34,11 +34,17 @@ with open('./lat_lon_by_zip_code.txt') as json_file:
 
 			response = requests.get(url).json()
 			results = response['results']
+			for i in range(len(results)):
+				result_entry = results[i]
+				result_keys = list(result_entry)
+				if('price_level' in result_keys):
+					priceLevel = result_entry['price_level']
+					print(priceLevel)
 			food_access_data[command['category']] = len(results)
-		print(food_access_data)
+		# print(food_access_data)
 		data[zipcode] = food_access_data
 
-print(data)
+#print(data)
 
 with open('food_proximity_data.txt', 'w') as outfile:
 	json.dump(data, outfile)
