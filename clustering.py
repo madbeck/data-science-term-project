9ee0ceffd883c56c7d13d4934dd13a2488675429
Maddie Becker <madeleine_becker@brown.edu>
2019-04-26 12:06:28 -0400

    Add graphs for food security vs. income

--- clustering.py
+++ clustering.py
@@ -17,24 +17,31 @@ def parse_data():
 	with open('./data/combined_data_by_zip_code/LA_Ranking_2019-04-04_21-54-40.csv') as combined_data:
 		csv_reader = csv.DictReader(combined_data, delimiter = ',')
 		data = []
+		zip_codes = []
 		for row in csv_reader:
 			cleaned_row = []
+			zip_codes.append(row['FIPS'])
 			cleaned_row.append(float(row['% Employment, Unemployed, 2018']))
 			cleaned_row.append(float(row['Median Household Income, 2018'])) # maybe flip this one?
 			cleaned_row.append(float(row['% Body mass index - Obese, 2018']))
 			cleaned_row.append(float(row['% Diabetes, 2018']))
 			data.append(np.array(cleaned_row))
 		data = np.array(data)
-	return data
+	return data, zip_codes
 
 ################################################################
 
-data = parse_data()
-kmeans = sk_learn_cluster(data, 4)
+data, zip_codes = parse_data()
+kmeans = sk_learn_cluster(data, 2)
 
 # cluster centers available at kmeans.cluster_centers_
 labels = kmeans.predict(data) # clusters each zip code is closest to
+print(zip_codes)
 print(labels)
 
-
+cluster1 = [zip_codes[i] for i in range(len(zip_codes)) if labels[i] == 1]
+cluster2 = [zip_codes[i] for i in range(len(zip_codes)) if labels[i] == 0]
+print(len(cluster1))
+print(len(cluster2))
+print(cluster1)
 
--- plot_food_security_and_income.py
+++ plot_food_security_and_income.py
@@ -1,4 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sqlite3
 
-# plotting food security on y axis and income on
+conn = sqlite3.connect('combined_data.db')
+c = conn.cursor()
+
+sql_command = "SELECT f.zip_code, f.one_km_sm, f.three_km_sm, f.one_km_ff, f.three_km_ff, e.income from food_security as f INNER JOIN economic_health_la as e on f.zip_code = e.zip_code;"
+c.execute(sql_command)
+result = c.fetchall()
+
+data = {}
+for r in result:
+    data[r[5]] = [r[1], r[2], r[3], r[4]]
+
+avg_income = sorted(data.keys())
+one_km_sm = [data[i][0] for i in avg_income]
+three_km_sm = [data[i][1] for i in avg_income]
+one_km_ff = [data[i][2] for i in avg_income]
+three_km_ff = [data[i][3] for i in avg_income]
+
+
+# PLOT DATA
+
+# plt.plot(avg_income, one_km_sm, label="1km supermarkets", color='blue', marker='o', linestyle='solid', linewidth=1, markersize=5)
+plt.plot(avg_income, one_km_ff, label="1km fast food", color='red', marker='o', linestyle='solid', linewidth=1, markersize=5)
+
+# plt.plot(avg_income, three_km_sm, '-b', label="3km supermarkets")
+# plt.plot(avg_income, three_km_ff, label="3km fast food")
+plt.xlabel('food security', fontsize=12)
+plt.ylabel('number of establishments', fontsize=12)
+plt.legend()
+plt.show()
+
+
+
+
+
